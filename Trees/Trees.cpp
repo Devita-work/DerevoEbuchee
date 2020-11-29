8a5cc3253dbc8234f6400b48fabc881840aba075
Vitali Metelev <devita.work@yandex.ru>
2020-11-29 18:22:50 +0400

    РАБОТАЕТ БЛЯТЬ

--- Trees/Trees.cpp
+++ Trees/Trees.cpp
@@ -18,29 +18,27 @@ struct TreeNode
 {
 	int info;
 	int sum; //добавил сумму, как характеристику вершины
-	TreeNode* left, * right;
+	TreeNode* left, * right, *back;
 };
 
 // Прототипы функций
-TreeNode* makeTree(int level);
+TreeNode* makeTree(int level, int b);
 void printTree(TreeNode*& root, const int level = 0);
 int sumTree(TreeNode* root);
 int countOddLevelNodes(TreeNode*& root, const int level = 0);
-int oldSumm(TreeNode*& root); // объявление функции поиска суммы предков (она работает, но пока только для 1 элемента :DDD)
+ // объявление функции поиска суммы предков (она работает, но пока только для 1 элемента :DDD)
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
 
+	int b = 0;
+
 	cout << "Введите дерево" << endl;
-	TreeNode* root = makeTree(0);
+	TreeNode* root = makeTree(0,b);
 
 	cout << "Введенное дерево:" << endl;
 	printTree(root);
-
-	cout << "Сумма вершин дерева = " << sumTree(root) << endl;
-
-	cout << "Количество вершин на нечётных уровнях = " << countOddLevelNodes(root, 0) << endl;
 
 	printTree(root->left);
 	//cout << endl << oldSumm(root) << endl;dsd
@@ -50,26 +48,25 @@ void main()
 }
 
 // Создание бинарного дерева вводом с клавиатуры
-TreeNode* makeTree(int level)
+TreeNode* makeTree(int level, int b)
 {
 	char c;
-	int b;
 	cout << setw(4 * level) << "" << "Создать вершину? (y/n)";
 	cin >> c;
 	if (c == 'y')
 	{
 		TreeNode* p = new TreeNode;
-		p->sum = 0;
+		
 		cout << setw(4 * level) << "" << "Введите значение вершины:";
 		cin >> p->info;
-		p->sum = p->sum + p->info; // на мой взгляд, можно считать сумму тут, но выводит кривые регистры, 
+		b = b + p->info; // на мой взгляд, можно считать сумму тут, но выводит кривые регистры, 
 		// не аккуратная работа с памятью, скорее всего надо занулить p->sum, что я и сделал в 62 строчке, но получается каждый раз сумма обнуляться будет, тоже калич крч
-
+		p->sum = b;
 		cout << setw(4 * level) << "" << "Левое поддерево вершины " << p->info << endl;
-		p->left = makeTree(level + 1);
+		p->left = makeTree(level + 1,b);
 
 		cout << setw(4 * level) << "" << "Правое поддерево вершины " << p->info << endl;
-		p->right = makeTree(level + 1);
+		p->right = makeTree(level + 1,b);
 		return p;
 	}
 	else
@@ -85,7 +82,7 @@ void printTree(TreeNode*& root, const int level)
 	if (root)
 	{
 		printTree(root->left, level + 1);
-		cout << setw(4 * level) << "" << root->info << root->sum << endl;
+		cout << setw(4 * level) << "" << root->info << "(" << root->sum - root->info << ")" << endl;
 		printTree(root->right, level + 1);
 	}
 	return;
@@ -107,18 +104,4 @@ int countOddLevelNodes(TreeNode*& root, const int level)
 		return (level % 2 == 1 ? 1 : 0) + countOddLevelNodes(root->left, level + 1) + countOddLevelNodes(root->right, level + 1);
 	else
 		return 0;
-}
-
-// Функция поиска суммы предков
-int oldSumm(TreeNode*& root)
-{
-	int rightSumm = 0;
-	int leftSumm = 0;
-	while ((root->left != NULL) and (root->right != NULL)) //пока у вершины есть корни делаем это:
-	{
-		if (root->left = NULL)
-		root = root->left;
-		leftSumm += root->info;
-	}
-	return leftSumm;
 }
